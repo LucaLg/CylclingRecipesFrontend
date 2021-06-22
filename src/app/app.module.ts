e6862d48a17567cf2e25373794370bf697f5ca00
luca <lucal.gno@gmail.com>
2021-06-22 17:21:07 +0200

    Created Main Components and Services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RecipesListComponent } from './Main/recipes-list/recipes-list.component';
+import { RecipeListComponent } from './Main/recipe-list/recipe-list.component';
+import { RecipeComponent } from './Main/recipe/recipe.component';
+import { NavbarComponent } from './header/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecipesListComponent,
+    RecipeListComponent,
+    RecipeComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
